3d2b6a8000cfb16bf570a3635b74000dfb1262f9
MELWIN JOSE <melwin.jose.1991@gmail.com>
2017-03-21 00:08:06 -0400

    Initial Layout

--- src/melwin/rkp/web-poc/recommendation.js
+++ src/melwin/rkp/web-poc/recommendation.js
@@ -1,3 +1,5 @@
+var DEBUG = true
+
 var contents = [
 	`Mohandas Karamchand Gandhi was born on 2 October 1869 in Porbandar. His father, 
 	Karamchand Uttamchand Gandhi (1822–1885), served as the diwan (chief minister) 
@@ -16,7 +18,7 @@ var contents = [
 	Born: October 2, 1869, Porbandar
 	Died: 30 January 1948`, 
 	
-	`Mohandas Karamchand Gandhi
+	`Name: Mohandas Karamchand Gandhi
 	Father: Karamchand Gandhi
 	Spouse: Kasturba Gandhi`
 ];
@@ -52,3 +54,94 @@ var triples = [
 		{subject : "Q1001", predicate : "P26", object : "Q264908" }
 	]
 ];
+
+var knowledge_profile = levelgraph(level("user-graph"));
+
+var content_graphs = [];
+content_graphs[0] = levelgraph(level("content-0"));
+content_graphs[1] = levelgraph(level("content-1"));
+content_graphs[2] = levelgraph(level("content-2"));
+content_graphs[3] = levelgraph(level("content-3"));
+content_graphs[4] = levelgraph(level("content-4"));
+
+function initGraph(content_index){
+	content_graphs[content_index].put(triples[content_index], function(err) {
+		if(DEBUG) console.log("inserted triples into graph-", content_index);
+		if(DEBUG) displayAll(content_graphs[content_index]);
+	});
+}
+
+function displayAll(graph){
+	graph.search({
+	    subject: graph.v("x"),
+	    predicate: graph.v("y"),
+	    object: graph.v("z")
+	  }, function(err, list) {
+			for(object of list){
+				if(DEBUG) console.log(object)
+			} 
+	  }
+	); 
+}
+
+function remove(graph, triple){
+	graph.del(triple, function(err) {
+		console.log("Removed ", triple, err)
+	});
+}
+
+function graphDifference(graph1, graph2){
+	// graph1 - graph2
+	graph2.search({
+	    subject: graph2.v("x"),
+	    predicate: graph2.v("y"),
+	    object: graph2.v("z")
+	  }, function(err, list) {
+			for(entry of list){
+				if(DEBUG) console.log("graph-2", entry)
+				triple = {
+					subject : entry["x"],
+					predicate : entry["y"],
+					object : entry["z"]
+				}
+				remove(graph1, triple)
+			} 
+	  }
+	 ); 
+	displayAll(graph1)
+}
+
+function add(graph, triple){
+	graph.put(triple, function(err) {
+		console.log("Added ", triple, err)
+	});
+}
+
+function addTo(graph1, graph2){
+	// graph1 =  graph1 + graph2
+	graph2.search({
+	    subject: graph2.v("x"),
+	    predicate: graph2.v("y"),
+	    object: graph2.v("z")
+	  }, function(err, list) {
+			for(entry of list){
+				if(DEBUG) console.log("graph-2", entry)
+				triple = {
+					subject : entry["x"],
+					predicate : entry["y"],
+					object : entry["z"]
+				}
+				add(graph1, triple)
+			} 
+	  }
+	 ); 
+	displayAll(graph1)
+}
+
+function initOnLoad(){
+	for (i = 0; i < 5; i++) { 
+		initGraph(0);
+		new_element = "<div id='Rank-"+i+"'>Article-"+i+" <button type='button'>Show >></button></div>"
+		document.getElementById("articles_recommendations_div").innerHTML += new_element;
+	}
+}
